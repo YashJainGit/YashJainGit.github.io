e38ccf01271ee8007463f89f13e15307a341e362
Yash Jain <ItIsYashJain@gmail.com>
2024-08-30 21:54:47 -0700

    removed

--- next.config.js
+++ next.config.js
@@ -3,11 +3,7 @@ import createMDX from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure `pageExtensions` to include markdown and MDX files
-<<<<<<< HEAD
   //basePath: "/yashjain128.github.io",
-=======
-  basePath: "/yashjain128.github.io",
->>>>>>> 322c645223e14023629f6b6b33b744289a66d339
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
