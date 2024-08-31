877e95808a076e31c4f5ee6e9c90f977223b847c
Yash Jain <ItIsYashJain@gmail.com>
2024-08-31 15:18:24 -0700

    Started formatting markdown

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
+import createMDX from '@next/mdx'
+ 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Configure `pageExtensions` to include markdown and MDX files
     output: "export",
-
-};
-
-export default nextConfig;
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+}
+ 
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+})
+ 
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
